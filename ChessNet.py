25f010454139608179e0b4e65549b3253f2a3d10
thinkpad <yasenvarf@gmail.com>
2025-10-30 18:23:07 +0100

    base NN

--- ChessNet.py
+++ ChessNet.py
@@ -1,6 +1,5 @@
 
 import torch.nn as nn
-import torch.nn.functional as F
 class ChessNet(nn.Module):
     #as far as i understand, nn.module need to have 2 methods: __init__ and forward
     #__init__ is for defining layers
@@ -10,12 +9,14 @@ class ChessNet(nn.Module):
         self.value_head = nn.Linear(320, 1) #so we need to flatten (20,4,4) to (320)
         self.policy_head = nn.Linear(320, 100)
         self.tan1 = nn.Tanh()
+        self.softmax = nn.Softmax(dim=1)
 
     #forward is for defining how data flows through the network
     def forward(self, x):
         x = self.conv1(x)
         common_features = x.view(-1, 320)  # (BatchSize, 320)
         value = self.value_head(common_features)  # (BatchSize, 1)
-        self.tan1(value)
+        value = self.tan1(value)
         policy = self.policy_head(common_features)  # (BatchSize, 100)
+        policy = self.softmax(policy)
         return value, policy #IMPORTANT: return both value and policy. at first i forgot to return policy and it was a pain to debug
