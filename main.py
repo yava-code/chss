0fafdbc9b839bca4b08883804eb9dcf3e49593c4
thinkpad <yasenvarf@gmail.com>
2025-10-30 16:26:04 +0100

    tensor translate

--- main.py
+++ main.py
@@ -24,3 +24,22 @@ print(np_array.shape)  # Should print (12, 8, 8)
 turn_layer = np.ones((8, 8), dtype=int) if board.turn == chess.WHITE else np.zeros((8, 8), dtype=int)
 np_array = np.concatenate((np_array, turn_layer[np.newaxis, :, :]), axis=0)
 print(np_array.shape)  # Should print (13, 8, 8)
+
+
+# Now we need to add 4 more layers for castling rights
+castling_checks = [
+    board.has_kingside_castling_rights(chess.WHITE),
+    board.has_queenside_castling_rights(chess.WHITE),
+    board.has_kingside_castling_rights(chess.BLACK),
+    board.has_queenside_castling_rights(chess.BLACK)
+]
+
+# For each of these, we will create an 8x8 layer filled with 1s if the right exists, or 0s if it does not.
+for has_right in castling_checks:
+    # Создаем слой 8x8 из 1 или 0
+    layer = np.ones((8, 8), dtype=int) if has_right else np.zeros((8, 8), dtype=int)
+
+
+    np_array = np.concatenate((np_array, layer[np.newaxis, :, :]), axis=0)
+
+print(np_array.shape)  # (17, 8, 8)
